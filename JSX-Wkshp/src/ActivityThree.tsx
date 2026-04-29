015e936db4cd7677d68f08ca7dbd65285444d5e4
NikEthernet <nitronik10@gmail.com>
2026-04-28 21:52:10 -0700

    Set-up for activity three

---
+++ JSX-Wkshp/src/ActivityThree.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  // TODO 1: Create a const that holds an array of 5 strings
+
+  return (
+    <div className="container">
+      <h1>My List</h1>
+
+      <ul className="item-list">
+        {/* TODO 2: Use the .map() method on your const to render 
+            each one as an <li> element. Remember to attach keys! */}
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
--- JSX-Wkshp/src/main.tsx
+++ JSX-Wkshp/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import ActThree from './ActivityThree.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ActThree />
   </StrictMode>,
 )
