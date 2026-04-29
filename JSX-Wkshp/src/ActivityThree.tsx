1a81cf883e6c568558436552d28736abc40aba6b
NikEthernet <nitronik10@gmail.com>
2026-04-28 22:00:56 -0700

    activity three finished

--- JSX-Wkshp/src/ActivityThree.tsx
+++ JSX-Wkshp/src/ActivityThree.tsx
@@ -1,16 +1,15 @@
-import './App.css';
+import './ActivityThree.css';
 
 function App() {
   // TODO 1: Create a const that holds an array of 5 strings
 
   return (
     <div className="container">
-      <h1>My List</h1>
+      <h1>My List :)</h1>
 
       <ul className="item-list">
         {/* TODO 2: Use the .map() method on your const to render 
             each one as an <li> element. Remember to attach keys! */}
-
       </ul>
     </div>
   );
